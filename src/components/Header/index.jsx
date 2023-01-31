88268ef1d1c50427e6c96e9d6a18e7dee5830449
Andres Laguilavo <lagui2003@gmail.com>
2023-01-31 09:14:14 -0500

    mejorando el input de busqueda

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react"
-import { NavLink, useNavigate } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import search from "../../assets/search.svg"
+import home from "../../assets/home.svg"
 import "../../styles/Header.css"
 
 export function Header() {
-  const [inputOpen, setInputOpen] = useState(false)
 
   return (
     <header className="header">
       <NavLink to="/">MoviesLaguilavo</NavLink>
       <section className="links-container">
-          <NavLink to={'/search'} className='link-search'>
-            <img src={search} alt='search' />
-            <p>Busqueda</p>
-          </NavLink>
+        <NavLink to="/" className='link-item--container'>
+          <img src={home} alt="search" />
+          <p className="link-item--info">Home</p>
+        </NavLink>
+        <NavLink to={"/search"} className="link-item--container">
+          <img src={search} alt="search" />
+          <p className="link-item--info">Busqueda</p>
+          {/* Busqueda */}
+        </NavLink>
       </section>
     </header>
   )
--- src/views/Search.jsx
+++ src/views/Search.jsx
@@ -1,41 +1,58 @@
 import React from "react"
-import '../styles/Search.css'
+import "../styles/Search.css"
 import { useState } from "react"
 import { Loading } from "../components/Loading"
 import { MoviePoster } from "../components/MoviePoster"
 import { useFetch } from "../hooks/useFetch"
+import ex from "../assets/delete.svg"
 
 export function Search() {
-
-  const [valueInput, setvalueInput] = useState('')
+  const [valueInput, setvalueInput] = useState("")
   let endpoint
-  let parameters = ''
-  
+  let parameters = ""
+
   //si el input esta en blanco muestra las que estan en trending
-  if (valueInput != '') {
-    endpoint = 'searched'
-    parameters = `&query=${valueInput}` 
-  }else{
-    endpoint = 'trending'
+  if (valueInput != "") {
+    endpoint = "searched"
+    parameters = `&query=${valueInput}`
+  } else {
+    endpoint = "trending"
   }
 
-  let { state, loading } = useFetch({ endpoint, queryParameters: parameters})
+  let { state, loading } = useFetch({ endpoint, queryParameters: parameters })
 
-  const handleSearch = (e)=>{
+  const handleSearch = (e) => {
     setvalueInput(e.target.value)
   }
-
+  const deleteSearch = (e) => {
+    setvalueInput("")
+  }
   return (
     <>
       <Loading loading={loading}>
-        <section>
-          <form action="">
-            <input type="text" value={valueInput} placeholder="buscar" onChange={handleSearch}/>
-            <button>buscar</button>
+        <section className="movie-search">
+          <h1>Busqueda</h1>
+          <form className="movie-search--form" onSubmit={deleteSearch}>
+            <input
+              type="text"
+              value={valueInput}
+              placeholder="Buscar"
+              onChange={handleSearch}
+            />
+            <button>
+              <img src={ex} className={`${valueInput == "" && "invisible"}`} />
+            </button>
           </form>
-            <h4>{state.results?.length != 0 && `Tus resultados para la busqueda de: ${valueInput}`}</h4>
+          <h4>
+            {state.results?.length != 0 &&
+              `Tus resultados para la busqueda de: ${valueInput}`}
+          </h4>
           <div className="movie-poster--container">
-            {state.results?.length === 0 && <h1>No se encontraron Resultados para la busqueda de: {valueInput}</h1>}
+            {state.results?.length === 0 && (
+              <h1>
+                No se encontraron Resultados para la busqueda de: {valueInput}
+              </h1>
+            )}
             {state.results?.map((movie) => (
               <MoviePoster
                 title={movie.title}
